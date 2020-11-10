69a1994315932b553f0d7d7eec89015e28bdbd36
VentureRice <57582125+VentureRice@users.noreply.github.com>
2020-11-10 10:26:17 +0800

    Add files via upload

--- Student_SQL50.sql
+++ Student_SQL50.sql
@@ -144,6 +144,27 @@ where t.t_name = '张三');
 
 -- 9、查询学过编号为"01"并且也学过编号为"02"的课程的同学的信息
 select st.* from student st
-right join score s1 on s1.s_id=st.s_id
-right join score s2 on s2.s_id=st.s_id
+inner join score s1 on s1.s_id=st.s_id
+inner join score s2 on s2.s_id=st.s_id
 where s1.c_id='01' and s2.c_id='02';
+
+select a.* 
+from
+    student a,
+    score b,
+    score c
+where
+    a.s_id = b.s_id
+    and a.s_id = c.s_id
+    and b.c_id = '01'
+    and c.c_id = '02';
+    
+-- 10、查询学过编号为"01"但是没有学过编号为"02"的课程的同学的信息
+select st.* from student st 
+inner join score sc on sc.s_id = st.s_id
+inner join course c on c.c_id=sc.c_id and c.c_id="01"
+where st.s_id not in (
+select st2.s_id from student st2 
+inner join score sc2 on sc2.s_id = st2.s_id
+inner join course c2 on c2.c_id=sc2.c_id and c2.c_id="02"
+);
